c6b091df743c8a92a8a08fb206fb0d41ebdd8222
Marco Joson <littlemjoson@gmail.com>
2026-03-24 14:13:19 -0700

    added some initial stuff for player and map

---
+++ wss/Scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+class_name player
+
+#max stats
+var maxStrength
+var maxWater
+var maxFood
+
+#current stats
+var currStrength
+var currWater
+var currFood
+var currGold
